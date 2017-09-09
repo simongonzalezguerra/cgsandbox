46752094f05da1faaafe0e26fa9a8f624bf4263d
Simón González Guerra <simon.gonzalez.guerra87@gmail.com>
2017-09-09 03:11:11 -0300

    Fixed fov calculations

--- cgs/src/utils.cpp
+++ cgs/src/utils.cpp
@@ -5,7 +5,6 @@ namespace cgs
 {
     float fov_to_fovy(float fov, float width, float height)
     {
-        float d = width / (2 * tanf(glm::radians(fov / 2)));
-        return 2 * glm::degrees(atan(height / (2 * d)));
+        return fov * (height / width); 
     }
 }
