916fa9218aeed0870365ade35023168a1a04d051
Simón González Guerra <simon.gonzalez.guerra87@gmail.com>
2018-02-02 02:30:41 -0300

    Added definition of user_id type in master branch since some uses of it seem to have crawled in from branch load_database_from_file breaking the build

--- rte/rte_common.hpp
+++ rte/rte_common.hpp
@@ -34,6 +34,17 @@ namespace rte
     //! @brief Type used to store a handle to a shader program (vertex and fragment shader). Non-zero.
     //-----------------------------------------------------------------------------------------------
     typedef unsigned int gl_program_id;
+
+    //-----------------------------------------------------------------------------------------------
+    //! @brief Type used to store an id that can be set by the user. Non-zero, optional, but unique
+    //-----------------------------------------------------------------------------------------------
+    typedef unsigned int user_id;
+
+    //-----------------------------------------------------------------------------------------------
+    //! @brief Constant representing 'not a user id'. This is the default user id assiged to
+    //!  objects that the user hasn't assigned an id to.
+    //-----------------------------------------------------------------------------------------------
+    constexpr user_id nuser_id = -1;
 } // namespace rte
 
 #endif // CGS_TYPES_HPP
