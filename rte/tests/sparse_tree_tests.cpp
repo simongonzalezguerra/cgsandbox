12a53371d2063c1d6f16738778cea8c2a9ec31e5
Simón González Guerra <simon.gonzalez.guerra87@gmail.com>
2018-02-08 01:07:50 -0300

    Added tests

--- rte/tests/sparse_tree_tests.cpp
+++ rte/tests/sparse_tree_tests.cpp
@@ -120,6 +120,11 @@ TEST_F(sparse_tree_test, insert_erase_search) {
     st.erase(7);
     std::cout << "tree after erasing node 7:\n";
     print_tree(st);
+
+    st.erase(2);
+    std::cout << "tree after erasing node 2:\n";
+    print_tree(st);
+
     search_result = find_value(7, st, search_index);
     ASSERT_EQ(search_result, false);
 }
@@ -134,7 +139,26 @@ TEST_F(sparse_tree_test, insert_erase_search) {
 //     7
 //     8
 TEST_F(sparse_tree_test, insert_tree) {
+    my_tree st;
+    std::vector<my_tree::value_type> nodes;
 
+    // my_tree::value_type node(nodes);
+    // node.m_elem = 1;
+    // node.m_parent = my_tree::value_type::npos;
+    // node.m_first_child = my_tree::value_type::npos;
+    // node.m_last_child = my_tree::value_type::npos;
+    // node.m_next_sibling = my_tree::value_type::npos;
+    // node.m_previous_sibling = my_tree::value_type::npos;
+    // nodes.push_back(node);
+
+    // node.m_elem = 2;
+    // node.m_parent = 1;
+    // node.m_first_child = my_tree::value_type::npos;
+    // node.m_last_child = my_tree::value_type::npos;
+    // node.m_next_sibling = my_tree::value_type::npos;
+    // node.m_previous_sibling = my_tree::value_type::npos;
+    // nodes.at(0)
+    // nodes.push_back(node);
 }
 
 int main(int argc, char** argv)
